d1d92774ec146e5b17ea4c69daa6dbf666f24395
Sanush Shakya <shakyasanush@7gmail.com>
2022-08-29 07:00:36 +0545

    Basic reactjs app tutorial

---
+++ test.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+      'name':"John Doe",
+      'age':"24",
+      'address':"kathmandu, Nepal"
+    }
+]
+const fetchData = () => {
+    for(let i=0; i<data.length; i++){
+        console.log(data)
+    }
+}
+
+fetchData();
